ad955d9daf801d900a81b75d67623601ab01ae9a
Devnilethegreat <212276580+Devnilethegreat@users.noreply.github.com>
2026-01-04 17:08:37 +0000

    Scaffold core module structure

---
+++ sentinelnet.py
@@ -0,0 +1,12 @@
+# sentinelnet.py
+"""
+Main module for SentinelNet application.
+"""
+
+import argparse
+import logging
+import os
+import sys
+from typing import Optional
+from dotenv import load_dotenv
+
